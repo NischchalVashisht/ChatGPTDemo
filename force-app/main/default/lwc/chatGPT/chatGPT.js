be56aec375554b705977ac725934ccefeda3841a
Nishchal vashisht <47364503+NischchalVashisht@users.noreply.github.com>
2023-03-01 14:12:39 +0530

    Update chatGPT.js

--- force-app/main/default/lwc/chatGPT/chatGPT.js
+++ force-app/main/default/lwc/chatGPT/chatGPT.js
@@ -29,7 +29,7 @@ export default class ChatGPT extends LightningElement {
                     this.responseData = this.responseData.replace(/\n/g, "<br />");
                     let tempScriptData = ''
                     tempScriptData = (response.choices[0].text.includes('<script>')) ? 'JS File: ' + response.choices[0].text.split('<script>')[1] : '';
-                    tempScriptData = this.responseTextLWCJS.replace(/\n/g, "<br />");
+                    tempScriptData = tempScriptData.replace(/\n/g, "<br />");
 
                     this.responseData = this.responseData + this.responseTextLWCJS;
                     this.responseData = (this.responseData.includes('XML File:')) ? this.responseData.split('XML File:')[0] : this.responseData;
